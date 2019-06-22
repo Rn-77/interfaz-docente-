d8385b0f10174a746e4aa07d604cbf4a6ffb8fff
Gaston Crinejo <49505032+D3v3loper1995@users.noreply.github.com>
2019-06-22 15:40:56 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ventana_login
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1_aceptar = new System.Windows.Forms.Button();
+            this.button1_salir = new System.Windows.Forms.Button();
+            this.btMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(8, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "ej. Gomez Daniel";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(8, 181);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(227, 20);
+            this.Password.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "No tienes cuenta? Registrate ahora ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 42);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "MI DOCENTE";
+            // 
+            // button1_aceptar
+            // 
+            this.button1_aceptar.Location = new System.Drawing.Point(-1, 287);
+            this.button1_aceptar.Name = "button1_aceptar";
+            this.button1_aceptar.Size = new System.Drawing.Size(126, 23);
+            this.button1_aceptar.TabIndex = 6;
+            this.button1_aceptar.Text = "Aceptar";
+            this.button1_aceptar.UseVisualStyleBackColor = true;
+            this.button1_aceptar.Click += new System.EventHandler(this.button1_aceptar_Click);
+            // 
+            // button1_salir
+            // 
+            this.button1_salir.Location = new System.Drawing.Point(120, 287);
+            this.button1_salir.Name = "button1_salir";
+            this.button1_salir.Size = new System.Drawing.Size(126, 23);
+            this.button1_salir.TabIndex = 7;
+            this.button1_salir.Text = "Salir";
+            this.button1_salir.UseVisualStyleBackColor = true;
+            this.button1_salir.Click += new System.EventHandler(this.button1_salir_Click);
+            // 
+            // btMostrar
+            // 
+            this.btMostrar.Location = new System.Drawing.Point(8, 233);
+            this.btMostrar.Name = "btMostrar";
+            this.btMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btMostrar.TabIndex = 8;
+            this.btMostrar.Text = "Mostrar BD";
+            this.btMostrar.UseVisualStyleBackColor = true;
+            this.btMostrar.Click += new System.EventHandler(this.btMostrar_Click);
+            // 
+            // ventana_login
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(247, 416);
+            this.Controls.Add(this.btMostrar);
+            this.Controls.Add(this.button1_salir);
+            this.Controls.Add(this.button1_aceptar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ventana_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1_aceptar;
+        private System.Windows.Forms.Button button1_salir;
+        private System.Windows.Forms.Button btMostrar;
+    }
+}
+
